a0990f299eebd8ce6e68955bdd7d3f69fbb306b8
jbowl <jbowles96@gmail.com>
2019-10-16 09:45:29 -0500

    MedicationModel and PatientModel added | Todo: Dbsets and migrations

---
+++ PrescriptionCapstone/PrescriptionCapstone/Models/Medication.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PrescriptionCapstone.Models
+{
+    public class Medication
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Generic Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Medication was Taken Today")]
+        public bool MedicationConfirmed { get; set; }
+
+        [Display(Name = "When to Take Medication")]
+        public DateTime ScheduledTimeToTake { get; set; }
+    }
+}
---
+++ PrescriptionCapstone/PrescriptionCapstone/Models/Patient.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace PrescriptionCapstone.Models
+{
+    public class Patient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Display(Name = "Email Address")]
+        public string EmailAddress { get; set; }
+
+        [Display(Name = "Diagnosis")]
+        public string Diagnosis { get; set; }
+
+        [Display(Name = "Current Medication")]
+        public List<Medication> CurrentListOfMedication { get; set; }
+
+        //[DataType(DataType.Date)]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Appointment")]
+        public DateTime? ScheduledAppointment { get; set; }
+
+        [Display(Name = "Log/Notes")]
+        public Dictionary<DateTime, string> Log { get; set; }
+
+        [ForeignKey("ApplicationUser")]
+        public string ApplicationId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
