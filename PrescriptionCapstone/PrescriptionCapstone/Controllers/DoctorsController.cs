c211d78620f3cc88fc4410951637c78266d7abf3
jbowl <jbowles96@gmail.com>
2019-10-16 11:10:07 -0500

    DoctorsController with read/write scaffolding

---
+++ PrescriptionCapstone/PrescriptionCapstone/Controllers/DoctorsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PrescriptionCapstone.Controllers
+{
+    public class DoctorsController : Controller
+    {
+        // GET: Doctors
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Doctors/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Doctors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Doctors/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Doctors/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Doctors/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Doctors/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Doctors/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
