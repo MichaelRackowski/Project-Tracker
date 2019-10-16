8201aacea50983ba6516018278198a806e042de0
Michael Rackowski <MichaelRackowski@gmail.com>
2019-10-16 10:19:05 -0500

    Ob Pull

---
+++ PrescriptionCapstone/PrescriptionAPI/PrescriptionAPI/Migrations/201910151846275_stuff.cs
@@ -0,0 +1,16 @@
+namespace PrescriptionAPI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class stuff : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
