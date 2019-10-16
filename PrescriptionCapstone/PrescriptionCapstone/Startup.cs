2d9f78eb0064f40ebdacb1cb9b6712a7ab5f13b7
Hannah Bingham <binghan.hb@gmail.com>
2019-10-16 09:29:29 -0500

    Started the set-up for user rolls.

--- PrescriptionCapstone/PrescriptionCapstone/Startup.cs
+++ PrescriptionCapstone/PrescriptionCapstone/Startup.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Owin;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Owin;
 using Owin;
+using PrescriptionCapstone.Models;
 
 [assembly: OwinStartupAttribute(typeof(PrescriptionCapstone.Startup))]
 namespace PrescriptionCapstone
@@ -9,6 +12,13 @@ namespace PrescriptionCapstone
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
+            createRolesandUsers();
+        }
+        private void createRolesandUsers()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManger = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context))
         }
     }
 }
